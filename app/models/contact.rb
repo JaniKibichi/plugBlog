4d1812a66ccab48e80a4d469e802808ee91f1a51
jani <janikibichi@gmail.com>
2015-12-22 13:00:27 -0500

    clean ups

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,10 +4,11 @@ class Contact < MailForm::Base
  attribute :message, :validate => true
  attribute :nickname, :captcha => true
 
- def headers{
-   :subject => "Contact Form",
-   :to => "info@pluginenergy.net",
-   :from => %("#{name}" <#{email}>)
-  }
+ def headers
+   {
+    :subject => "Contact Form",
+    :to => "info@pluginenergy.net",
+    :from => %("#{name}" <#{email}>)
+   }
  end
 end
