b6c1e2f5ee8dc689b46c71b652fdba6098fab9be
jani <janikibichi@gmail.com>
2015-12-22 11:49:29 -0500

    contact

--- Gemfile
+++ Gemfile
@@ -29,8 +29,6 @@ gem 'will_paginate', '~>3.0.7'
 gem 'mail_form', '~>1.5.1'
 gem 'devise', '~>3.4.1'
 
-
-
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   resources :projects
-
-  get 'welcome/index'
+  resources :contacts, only: [:new, :create]
+ 
+ get 'welcome/index'
   root 'welcome#index'
 
   get '*path' => redirect('/')
